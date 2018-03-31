7329abbf468c796056abf669d1fc6aff6dfdc29d
Adam Sheets <adamsheets89@gmail.com>
2018-03-31 09:59:30 -0500

    added photo and links

--- javascript/portfolio.js
+++ javascript/portfolio.js
@@ -2,10 +2,12 @@ $("#projects").on("click", function (event) {
     event.preventDefault();
     $(".second-row").hide();
     $(".third-row").show();
+    $(".fourth-row").show();
 });
 
 $("#home").on("click", function (event) {
     event.preventDefault();
     $(".second-row").show();
     $(".third-row").hide();
+    $(".fourth-row").hide();
 });
