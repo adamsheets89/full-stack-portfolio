8c5cb57d6c23a831bdc9a802df5eaa8250e6f3ab
Adam Sheets <adamsheets89@gmail.com>
2018-03-31 19:50:57 -0500

    styling

--- javascript/portfolio.js
+++ javascript/portfolio.js
@@ -1,13 +1,13 @@
-$("#projects").on("click", function (event) {
-    event.preventDefault();
-    $(".second-row").hide();
-    $(".third-row").show();
-    $(".fourth-row").show();
-});
+// $("#projects").on("click", function (event) {
+//     event.preventDefault();
+//     $(".second-row").hide();
+//     $(".third-row").show();
+//     $(".fourth-row").show();
+// });
 
-$("#home").on("click", function (event) {
-    event.preventDefault();
-    $(".second-row").show();
-    $(".third-row").hide();
-    $(".fourth-row").hide();
-});
+// $("#home").on("click", function (event) {
+//     event.preventDefault();
+//     $(".second-row").show();
+//     $(".third-row").hide();
+//     $(".fourth-row").hide();
+// });
